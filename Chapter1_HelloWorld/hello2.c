da2efc4f194c0d46e6e2ef0f748e726d10ae20aa
Carolina Rolo <organa.skyw@gmail.com>
2022-01-04 16:04:16 -0300

    First Commit

---
+++ Chapter1_HelloWorld/hello2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/*
+ * hello.c
+ * My First C Program with comments
+ * by Carol Rolo
+ * created 2022/04/01
+ */
+
+int main() {
+  printf("Hello, World 2!\n");
+  return 0;
+}
+
+/* eof */
